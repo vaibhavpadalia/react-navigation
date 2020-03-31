c9dc336a63d20b9498087e6fc3a5fdc18d2b6dc6
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2020-03-31 16:16:52 +0530

    Navigation integration completed

--- App.js
+++ App.js
@@ -3,21 +3,30 @@ import Login from "./src/components/Login";
 import Signup from "./src/components/Signup";
 import { TouchableOpacity } from "react-native";
 import Dashboard from "./src/components/Dashboard";
-import { Icon } from "react-native-elements";
-
+// import { Icon } from "react-native-elements";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react/navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const AuthStack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
-
+const Drawer = createDrawerNavigator();
 
 const AuthStackScreen = () => {
   return (
     <AuthStack.Navigator>
-      <AuthStack.Screen name={"Tab"} component={Tabs} />
-      <AuthStack.Screen name={"Dashboard"} component={Dashboard} />
+      <AuthStack.Screen name={"Test App"} component={TabsScreen} options={{
+        drawerLockMode: "locked-closed",
+        headerStyle: { backgroundColor: "#173746" },
+        headerTitleStyle: { color: 'white' },
+        drawerLockMode: 'locked-closed'
+      }} />
+      <AuthStack.Screen name={"Dashboard"} component={Dashboard} options={{
+        headerStyle: { backgroundColor: "#173746" },
+        headerTintColor: "white",
+        headerTitleStyle: { color: 'white' },
+      }} />
     </AuthStack.Navigator>
   );
 };
@@ -25,51 +34,21 @@ const AuthStackScreen = () => {
 
 const TabsScreen = () => {
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator tabBarOptions={{ activeTintColor: "white", style: { backgroundColor: "#173746" } }} >
       <Tabs.Screen name={"Login"} component={Login} />
       <Tabs.Screen name={"Signup"} component={Signup} />
-    </Tabs.Navigator>
+    </Tabs.Navigator >
   );
 };
 
-
-
-// const tabNav = TabNavigator(
-//   {
-//     Login: { screen: Login },
-//     Signup: { screen: Signup }
-//   },
-//   {
-//     tabBarPosition: "bottom",
-//     tabBarOptions: {
-//       style: {
-//         backgroundColor: "#173746"
-//       }
-//     },
-//     lazyLoad: true
-//   }
-// );
-
-// const stackNav = StackNavigator({
-//   Tab: { screen: tabNav },
-//   Dashboard: {
-//     screen: Dashboard,
-//     navigationOptions: ({ navigation }) => ({
-//       title: "Dashboard",
-//       headerTitleStyle: { color: "white" },
-//       headerTintColor: "white",
-//       headerLeft: (
-//         <TouchableOpacity onPress={() => navigation.navigate("DrawerOpen")}>
-//           <Icon name="menu" size={30} color="white" />
-//         </TouchableOpacity>
-//       ),
-//       headerStyle: { paddingRight: 10, paddingLeft: 10, backgroundColor: "#173746" }
-//     })
-//   }
-// });
-
-// const App = DrawerNavigator({
-//   Home: { screen: stackNav }
-// });
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name={"Authentication Stack"} component={AuthStackScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,73 +1,63 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
-import { Header, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
+import { View, Text, ScrollView, StyleSheet, Button, TextInput } from 'react-native';
 
 class Signup extends Component {
-	static navigationOptions = {
-		headerTitle: 'Test App',
-		headerStyle: { backgroundColor: '#173746' },
-		headerTitleStyle: { color: 'white' },
-		drawerLockMode: 'locked-closed'
-	};
+  state = {
+    email: '',
+    password: ''
+  };
 
-	state = {
-		email: '',
-		password: ''
-	};
+  emailValidation = () => {
+    if (this.state.email.length === 0) {
+      return <Text style={{ color: "red" }}>Email address is required</Text>;
+    } else {
+      return null;
+    }
+  };
 
-	emailValidation = () => {
-		if (this.state.email.length === 0) {
-			return <FormValidationMessage>Email address is required</FormValidationMessage>;
-		} else {
-			return null;
-		}
-	};
+  passwordValidation = () => {
+    if (this.state.password.length === 0) {
+      return <Text style={{ color: "red" }}>Password is required</Text>;
+    } else {
+      return null;
+    }
+  };
 
-	passwordValidation = () => {
-		if (this.state.password.length === 0) {
-			return <FormValidationMessage>Password is required</FormValidationMessage>;
-		} else {
-			return null;
-		}
-	};
-
-	render() {
-		const { navigate } = this.props.navigation;
-		return (
-			<ScrollView keyboardShouldPersistTaps="always">
-				<View style={styles.container}>
-					<FormLabel>Email:</FormLabel>
-					<FormInput placeholder="Enter email" onChangeText={(text) => this.setState({ email: text })} />
-					{this.emailValidation()}
-					<FormLabel>Password:</FormLabel>
-					<FormInput
-						placeholder="Enter password"
-						onChangeText={(text) => this.setState({ password: text })}
-					/>
-					{this.passwordValidation()}
-					<View style={styles.submitStyle}>
-						<Button
-							title="SIGNNUP"
-							raised
-							icon={{ name: 'code' }}
-							backgroundColor="#173746"
-							onPress={() => navigate('Dashboard')}
-						/>
-					</View>
-				</View>
-			</ScrollView>
-		);
-	}
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <ScrollView keyboardShouldPersistTaps="always" style={{ margin: 10 }}>
+        <View style={styles.container}>
+          <Text>Email:</Text>
+          <TextInput style={styles.textInput} placeholder="Enter email" onChangeText={(text) => this.setState({ email: text })} />
+          {this.emailValidation()}
+          <Text>Password:</Text>
+          <TextInput style={styles.textInput} placeholder="Enter password" onChangeText={(text) => this.setState({ password: text })}
+          />
+          {this.passwordValidation()}
+          <View style={styles.submitStyle}>
+            <Button title="SIGN UP" onPress={() => navigate('Dashboard')} />
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		marginTop: 30,
-		marginBottom: 20
-	},
-	submitStyle: {
-		marginTop: 30
-	}
+  container: {
+    marginTop: 30,
+    marginBottom: 20
+  },
+  submitStyle: {
+    marginTop: 30
+  },
+  textInput: {
+    margin: 5,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5
+  }
 });
 
 export default Signup;
