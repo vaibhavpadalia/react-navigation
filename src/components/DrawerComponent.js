094d2a122cd5af44b77f2c3733124f75cbfd1a79
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2020-04-06 12:08:02 +0530

    Changes in drawer component

---
+++ src/components/DrawerComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-nativea";
+
+const DrawerComponent = (props) => {
+  return (
+    <View>
+      <Text>{"Test"}</Text>
+    </View>
+  );
+};
+
+export default DrawerComponent;
