83a35d2d9905e8ecaa460584463dc7cd5f4937f5
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2020-04-11 20:32:43 +0530

    Formatting changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,8 @@ class Home extends Component {
     return (
       <View style={{ alignItems: "center" }}>
         <Text style={{ marginTop: 30, marginBottom: 30 }}>Home Page</Text>
-        <Image style={{ height: 200, width: 200 }} resizeMode={"contain"} source={require("../images/SignupLogo.png")} />
+        <Image resizeMode={"contain"} source={require("../images/SignupLogo.png")}
+          style={{ height: 200, width: 200, flexDirection: "row" }} />
       </View>
     );
   }
