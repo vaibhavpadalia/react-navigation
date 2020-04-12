c836ea1721596ce5536e1eef947a306d44c24bed
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2020-04-12 23:36:50 +0530

    Icons added to tab

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Image } from "react-native";
 import Login from "./src/components/Login";
 import Signup from "./src/components/Signup";
 import Dashboard from "./src/components/Dashboard";
@@ -30,7 +31,19 @@ const AuthStackScreen = () => {
 
 const TabsScreen = () => {
   return (
-    <Tabs.Navigator tabBarOptions={{ activeTintColor: "white", style: { backgroundColor: "#173746" } }} >
+    <Tabs.Navigator
+      tabBarOptions={{ activeTintColor: "red", style: { backgroundColor: "#173746" } }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Login') {
+            iconName = require("./src/images/login.png");
+          } else if (route.name === 'Signup') {
+            iconName = require("./src/images/signup.png");
+          }
+          return <Image source={iconName} style={{ height: 25, width: 25 }} />;
+        },
+      })} >
       <Tabs.Screen name={"Login"} component={Login} />
       <Tabs.Screen name={"Signup"} component={Signup} />
     </Tabs.Navigator >
